4c5b428d7e5e9f637f958242d1781bd70f29406e
SnowCait <SnowCait@users.noreply.github.com>
2023-02-18 09:19:55 +0900

    npm run prepare

--- dist/index.js
+++ dist/index.js
@@ -8830,9 +8830,9 @@ async function run() {
   try {
     const relay = core.getInput('relay');
     const privateKey = core.getInput('private-key');
-    const payload = core.getInput('payload');
+    const content = core.getInput('content');
     core.setSecret(privateKey);
-    await nostr(relay, privateKey, payload);
+    await nostr(relay, privateKey, content);
   } catch (error) {
     core.setFailed(error.message);
   }
