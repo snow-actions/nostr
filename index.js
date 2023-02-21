855c8bfc1b74048e60c6a41d1cefeed9a7b9f5af
SnowCait <SnowCait@users.noreply.github.com>
2023-02-22 08:02:55 +0900

    Log

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { createMessage, postMessage } = require('./nostr');
 async function run() {
   try {
     const relays = core.getInput('relays').split("\n").map(x => x.trim()).filter(x => x.startsWith('wss://'));
+    core.info(relays.join("\n"))
     const privateKey = core.getInput('private-key');
     const content = core.getInput('content');
     core.setSecret(privateKey);
