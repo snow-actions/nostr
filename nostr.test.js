4aa426afdd78dccfded4f4e42a01caaa8ac4a8b6
SnowCait <SnowCait@users.noreply.github.com>
2023-02-22 08:16:14 +0900

    Fix typo

--- nostr.test.js
+++ nostr.test.js
@@ -2,9 +2,9 @@ const { createMessage, postMessage } = require('./nostr');
 require('dotenv').config();
 
 test('test createMessage', async () => {
-  const privatekey = process.env.NOSTR_PRIVATE_KEY;
+  const privateKey = process.env.NOSTR_PRIVATE_KEY;
   const content = 'test';
-  const message = await createMessage(privatekey, content);
+  const message = await createMessage(privateKey, content);
   const [ type, event ] = JSON.parse(message);
   expect(type).toBe('EVENT');
   expect(event).toHaveProperty('id');
@@ -19,9 +19,9 @@ test('test createMessage', async () => {
 
 test('test', async () => {
   const relays = process.env.NOSTR_RELAYS.split("\n").map(x => x.trim()).filter(x => x.startsWith('wss://'));
-  const privatekey = process.env.NOSTR_PRIVATE_KEY;
+  const privateKey = process.env.NOSTR_PRIVATE_KEY;
   const content = 'test';
-  const message = await createMessage(privatekey, content);
+  const message = await createMessage(privateKey, content);
   for (const relay of relays) {
     await postMessage(relay, message);
   }
