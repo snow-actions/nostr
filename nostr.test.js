213db3eeb78e21d3e7dde89f44eab00e0d1418b5
SnowCait <SnowCait@users.noreply.github.com>
2025-11-22 14:43:23 +0900

    Format

--- nostr.test.js
+++ nostr.test.js
@@ -1,45 +1,47 @@
-const { createEvent, publishEvent } = require('./nostr');
-require('dotenv').config();
+const { createEvent, publishEvent } = require("./nostr");
+require("dotenv").config();
 
-test('createEvent with nsec', async () => {
+test("createEvent with nsec", async () => {
   const privateKey = process.env.NOSTR_PRIVATE_KEY;
-  const content = 'test';
+  const content = "test";
   const kind = 1;
   const tags = [];
   const event = createEvent(privateKey, kind, content, tags);
-  expect(event).toHaveProperty('id');
-  expect(event).toHaveProperty('pubkey');
-  expect(event).toHaveProperty('created_at');
-  expect(event).toHaveProperty('kind');
-  expect(event).toHaveProperty('tags');
-  expect(event).toHaveProperty('content');
-  expect(event).toHaveProperty('sig');
+  expect(event).toHaveProperty("id");
+  expect(event).toHaveProperty("pubkey");
+  expect(event).toHaveProperty("created_at");
+  expect(event).toHaveProperty("kind");
+  expect(event).toHaveProperty("tags");
+  expect(event).toHaveProperty("content");
+  expect(event).toHaveProperty("sig");
   expect(event.content).toBe(content);
 });
 
-test('createEvent with seckey', async () => {
+test("createEvent with seckey", async () => {
   const privateKey = process.env.NOSTR_SECKEY;
-  const content = 'test';
+  const content = "test";
   const kind = 1;
   const tags = [];
   const event = createEvent(privateKey, kind, content, tags);
-  expect(event).toHaveProperty('id');
-  expect(event).toHaveProperty('pubkey');
-  expect(event).toHaveProperty('created_at');
-  expect(event).toHaveProperty('kind');
-  expect(event).toHaveProperty('tags');
-  expect(event).toHaveProperty('content');
-  expect(event).toHaveProperty('sig');
+  expect(event).toHaveProperty("id");
+  expect(event).toHaveProperty("pubkey");
+  expect(event).toHaveProperty("created_at");
+  expect(event).toHaveProperty("kind");
+  expect(event).toHaveProperty("tags");
+  expect(event).toHaveProperty("content");
+  expect(event).toHaveProperty("sig");
   expect(event.content).toBe(content);
 });
 
-test('publishEvent', async () => {
-  const relays = process.env.NOSTR_RELAYS.split("\n").map(x => x.trim()).filter(x => x.startsWith('wss://'));
+test("publishEvent", async () => {
+  const relays = process.env.NOSTR_RELAYS.split("\n")
+    .map((x) => x.trim())
+    .filter((x) => x.startsWith("wss://"));
   const privateKey = process.env.NOSTR_PRIVATE_KEY;
   const kind = 1;
   expect(privateKey).toBeDefined();
-  expect(privateKey).not.toBe('');
-  const content = 'test';
+  expect(privateKey).not.toBe("");
+  const content = "test";
   const tags = [];
   const event = createEvent(privateKey, kind, content, tags);
   await publishEvent(relays, event);
