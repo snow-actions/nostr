c966d47d2080632e63dbae8ae85d9f50321fa4ce
雪猫 <SnowCait@users.noreply.github.com>
2026-08-25 04:43:30 +0000

    Migrate ESLint to flat config

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: ['dist/**'],
+  },
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+];
